b8ff238229f114b15e971bc314d3b771d859cfe8
akkady <akkady43@gmail.com>
2021-01-28 17:29:51 +0100

    add templete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import AdminMain from "./admin-template/AdminMain"
 
 function App() {
   return (
     <div className="App">
-      <h1>hello wold </h1>
+      <AdminMain />
     </div>
   );
 }
