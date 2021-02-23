f944abadab6ba5a47793f2f8bcf386e46710d799
Brahim ED-DRAOUY <58387995+DRAOUY@users.noreply.github.com>
2021-02-23 19:36:02 +0100

    Update AddCategorieComponent.jsx

--- src/admin-template/AddCategorieComponent.jsx
+++ src/admin-template/AddCategorieComponent.jsx
@@ -1,30 +1,55 @@
-
 import React  from 'react'; 
+import CategorieService from '../Api/Service';
 
-export default  function AddCategorieComponent (){
+class AddCategorie extends React.Component{
 
-   
+    categorie  = {
+        nomCategorie :'',
+        slag :'',
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+          item: this.categorie
+        };
+        this.AddCategorie = this.AddCategorie.bind(this);
+      }
+      AddCategorie = (e) => {
+        e.preventDefault();
+        let categorie = {nomCategorie: this.state.nomCategorie, slag: this.state.slag};
+        CategorieService.addCategorie(categorie)
+            .then(res => {
+                
+                this.props.history.push('AdminMain/categories');
+            });
+
+    }
+    onChange = (e) =>
+        this.setState({ [e.target.name]: e.target.value });
+      render(){
+        const {item} = this.state;
+
       return (
         <div className="card ">
        <div className="card shadow mb-3">
           <div className="card-header py-3">
-              <p className="text-primary m-0 font-weight-bold">New Categorie</p>
+              <p className="text-primary m-0 font-weight-bold">Ajouter Categorie</p>
           </div>
           <div className="card-body">
               <form>
                   <div className="form-row">
                       <div className="col">
                           <div className="form-group">
-                            <label htmlFor="username"><strong>id</strong></label><input className="form-control" type="number" placeholder="id categorie" name="idCategorie"/>
+                            <label htmlFor="cateroriername"><strong>Categorie Name</strong></label><input className="form-control" type="text" placeholder="categorie.name" name="nomCategorie" value={this.state.nomCategorie} onChange={this.onChange}/>
                           </div>
                       <div>
-                          <div className="form-group"><label htmlFor="détails"><strong>name</strong></label><input className="form-control" type="text" placeholder="categorie name" name="categorie"/></div>
-                          <div className="form-group"><label htmlFor="Quantity"><strong>slug</strong></label><input className="form-control" type="text" placeholder="slug" name="slug"/></div>
+                          <div className="form-group"><label htmlFor="Slag"><strong>Slag</strong></label><input className="form-control" type="text" placeholder="Slag" name="slag" value={this.state.slag} onChange={this.onChange}/></div>
+                        
                        </div>
                        <div></div>
                       </div>
                   </div>
-                  <div className="form-group"><button className="btn btn-primary btn-sm" type="submit">save</button></div>
+                  <div className="form-group"><button className="btn btn-primary btn-sm" type="submit"  onClick={this.AddCategorie}>Ajout</button></div>
               </form>
           </div>
        </div>
@@ -32,3 +57,6 @@ export default  function AddCategorieComponent (){
        </div>
       );
     }
+    }
+    export default AddCategorie;
+   
